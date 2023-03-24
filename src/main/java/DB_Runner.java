5b78c03703751c9fbbb50eea8544a8a4421e7e54
GBatro <liverman214@gmail.com>
2023-03-24 14:43:56 -0400

    Added an archive system

--- src/main/java/DB_Runner.java
+++ src/main/java/DB_Runner.java
@@ -1,8 +1,6 @@
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.math.BigDecimal;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -12,9 +10,13 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class DB_Runner {
 
     private static final CommQueries cq = new CommQueries();
-    private static final String commLoc = "C:\\Users\\blake\\Downloads\\";
     private static final Scanner sc = new Scanner(System.in);
     private static String[] names;
+    private static List invoiceList = new ArrayList<Integer>();
+    private static List invoiceListFile = new ArrayList<Integer>();
+
+    //Location where related files can be found
+    private static final String fileLocations = "C:\\Users\\blake\\Downloads\\";
 
     public static void main(String[] args) {
         int numWorkbooks;
@@ -24,12 +26,12 @@ public class DB_Runner {
         sc.nextLine();
         names = new String[numWorkbooks];
 
-        System.out.println("Enter names below:");
+        System.out.println("Enter names (filenames, specifically) below:");
         for(int x = 0; x < numWorkbooks; x++) {
             names[x] = sc.nextLine();
         }
 
-        sc.close();
+        readTxt("archive.txt");
 
         for(int x = 0; x < numWorkbooks; x++) {
             System.out.println("Showing results for " + names[x] + "'s workbook:");
@@ -44,10 +46,20 @@ public class DB_Runner {
 
             System.out.println();
         }
+
+        System.out.println("Do you want to archive now? (y/n)");
+        String archive;
+
+        archive = sc.next();
+        sc.close();
+
+        if(archive.compareTo("y") == 0) {
+            saveToTxt("archive.txt");
+        }
     }
 
     private static Workbook setUpWorkbook(String name) throws IOException {
-        FileInputStream inputStream = new FileInputStream(commLoc+name+".xlsx");
+        FileInputStream inputStream = new FileInputStream(fileLocations +name+".xlsx");
         Workbook workbook = new XSSFWorkbook(inputStream);
         inputStream.close();
 
@@ -55,7 +67,7 @@ public class DB_Runner {
     }
 
     private static void wb2dbCompare(Workbook workbook, String salesPerson) {
-        int invoice;
+        Integer invoice;
         CommRow commRow;
 
         //Row iterator
@@ -94,6 +106,45 @@ public class DB_Runner {
             commRow.setSalesPerson((int)cell.getNumericCellValue());
 
             System.out.print(cq.compareInfo(invoice, commRow, salesPerson));
+            if(isInArchive(invoice)) {
+                System.out.println("---------- " + invoice + " was found to be in an old commission sheet.");
+            }
+            invoiceList.add(invoice);
         }
+    }
+
+    private static void saveToTxt(String fileName) {
+        invoiceList.addAll(invoiceListFile);
+        Collections.sort(invoiceList);
+        int size = invoiceList.size();
+
+        try {
+            FileWriter fw = new FileWriter(new File(fileLocations + fileName));
+            for (int i = 0; i < size; i++) {
+                fw.write(invoiceList.remove(0) + "\n");
+            }
+            fw.close();
+        } catch(IOException e) {
+            System.out.println("File \"" + fileName + "\" was not found or was not accessible.");
+        }
+    }
+
+    private static void readTxt(String fileName) {
+        try
+        {
+            Scanner scan = new Scanner(new File(fileLocations + fileName));
+            while(scan.hasNextInt())
+            {
+                invoiceListFile.add(scan.nextInt());
+            }
+        }
+        catch (IOException fe)
+        {
+            System.out.println("File \"" + fileName + "\" was not found or was not accessible.");
+        }
+    }
+
+    private static boolean isInArchive(Integer invoice) {
+        return invoiceListFile.contains(invoice);
     }
 }
